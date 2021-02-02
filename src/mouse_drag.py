2a7e38aedbdf6b57fe1343f99a7ed1a0bcb19066
Chae Seong Min <fire5450@naver.com>
2021-02-02 18:13:07 +0900

    RGB 최솟값을 추출하는 함수 추가

--- src/mouse_drag.py
+++ src/mouse_drag.py
@@ -2,6 +2,7 @@
 import sys
 
 import cv2
+import numpy as np
 
 from PyQt5.QtGui import QPixmap, QImage, QPainter, QBrush, QColor, QPen, QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDesktopWidget, QVBoxLayout, QWidget, QLabel
@@ -19,6 +20,10 @@ class Js06MainWindow(Ui_MainWindow):
         self.begin = QPoint()
         self.end = QPoint()
         self.qt_img = QPixmap()
+        self.isDrawing = True
+
+        self.upper_left = ()
+        self.lower_right = ()
 
     def setupUi(self, MainWindow: QMainWindow):
         super().setupUi(MainWindow)
@@ -50,12 +55,16 @@ class Js06MainWindow(Ui_MainWindow):
     def convert_cv_qt(self, cv_img):
         """Convert from an opencv image to QPixmap"""
         rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        self.cp_image = rgb_image.copy()
         self.img_height, self.img_width, ch = rgb_image.shape
         # print(self.img_height, self.img_width)
         self.label_width = self.image_label.width()
         self.label_height = self.image_label.height()
-
         bytes_per_line = ch * self.img_width
+        rec_color = (139, 0, 255)
+        if len(self.lower_right) > 0:
+            cv2.rectangle(rgb_image, self.upper_left, self.lower_right, rec_color, 6)
+
         convert_to_Qt_format = QImage(rgb_image.data, self.img_width, self.img_height, bytes_per_line,
                                             QImage.Format_RGB888)
         p = convert_to_Qt_format.scaled(self.image_label.width(), self.image_label.height(), Qt.KeepAspectRatio,
@@ -66,29 +75,66 @@ class Js06MainWindow(Ui_MainWindow):
         """레이블 위에 그림을 그리는 함수, QLabel method overriding"""
         qp = QPainter(self.image_label)
         qp.drawPixmap(self.image_label.rect(), self.qt_img)
-        br = QBrush(QColor(100, 10, 10, 40))
-        qp.setBrush(br)
-        qp.setPen(QPen(Qt.red, 2, Qt.SolidLine))
-        qp.drawRect(QRect(self.begin, self.end))
+
+        if self.isDrawing:
+            br = QBrush(QColor(100, 10, 10, 40))
+            qp.setBrush(br)
+            qp.setPen(QPen(Qt.red, 2, Qt.SolidLine))
+            qp.drawRect(QRect(self.begin, self.end))
 
     def mousePressEvent(self, event):
         """마우스 클릭시 발생하는 이벤트, QLabel method overriding"""
-        self.begin = event.pos()
-        self.end = event.pos()
-        print(self.begin)
-        self.image_label.update()
+        if event.buttons() == Qt.LeftButton:
+            self.isDrawing = True
+            self.begin = event.pos()
+            self.end = event.pos()
+            self.upper_left = (int((self.begin.x()/self.label_width)*self.img_width), int((self.begin.y()/self.label_height)*self.img_height))
+            print(self.begin)
+            self.image_label.update()
+
+        elif event.buttons() == Qt.RightButton:
+            self.isDrawing = False
+            pass
 
     def mouseMoveEvent(self, event):
         """마우스가 움직일 때 발생하는 이벤트, QLabel method overriding"""
-        self.end = event.pos()
-        self.image_label.update()
+        if event.buttons() == Qt.LeftButton:
+            self.end = event.pos()
+            self.image_label.update()
+            self.isDrawing = True
 
     def mouseReleaseEvent(self, event):
         """마우스 클릭이 떼질 때 발생하는 이벤트, QLabel method overriding"""
         self.end = event.pos()
         self.image_label.update()
         print(self.end.x(), self.end.y())
+        self.lower_right = (int((self.end.x()/self.label_width)*self.img_width), int((self.end.y()/self.label_height)*self.img_height))
+        self.minrgb()
+        self.isDrawing = False
 
+    def minrgb(self):
+        """드래그한 영역의 RGB 최솟값을 추출한다"""
+        left_y = min(self.upper_left[1], self.lower_right[1])
+        right_y = max(self.upper_left[1], self.lower_right[1])
+
+        left_x = min(self.upper_left[0], self.lower_right[0])
+        right_x = max(self.upper_left[0], self.lower_right[0])
+
+        test = self.cp_image[left_y:right_y, left_x:right_x, :]
+
+        # 드래그한 영역의 RGB 값을 각각 추출한다.
+        r = test[:, :, 0]
+        g = test[:, :, 1]
+        b = test[:, :, 2]
+
+        # RGB값을 각 위치별로 모두 더한다.
+        # RGB 최댓값이 255로 정해져있어 값을 초과하면 0부터 시작된다. numpy의 clip 함수를 이용해 array의 최댓값을 수정한다.
+        r = np.clip(r, 0, 765)
+        sum_rgb = r + g + b
+
+        # RGB 값을 합한 뒤 가장 최솟값의 index를 추출한다.
+        t_idx = np.where(sum_rgb == np.min(sum_rgb))
+        print(t_idx[0][0])
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
