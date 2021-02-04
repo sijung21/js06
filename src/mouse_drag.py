4a1b6547abc74e82aaa10410e918c2b6e750390a
Chae Seong Min <fire5450@naver.com>
2021-02-04 16:15:00 +0900

    계산한 이미지 좌표 값 출력

--- src/mouse_drag.py
+++ src/mouse_drag.py
@@ -24,6 +24,10 @@ class Js06MainWindow(Ui_MainWindow):
 
         self.upper_left = ()
         self.lower_right = ()
+        self.a = ()
+        self.b = ()
+        self.target_x = []
+        self.target_y = []
 
     def setupUi(self, MainWindow: QMainWindow):
         super().setupUi(MainWindow)
@@ -62,8 +66,14 @@ class Js06MainWindow(Ui_MainWindow):
         self.label_height = self.image_label.height()
         bytes_per_line = ch * self.img_width
         rec_color = (139, 0, 255)
-        if len(self.lower_right) > 0:
-            cv2.rectangle(rgb_image, self.upper_left, self.lower_right, rec_color, 6)
+
+        if len(self.a) > 0:
+            cv2.rectangle(rgb_image, self.a, self.b, rec_color, 6)
+
+        tar_color = (0, 255, 0)
+        if len(self.target_x) > 0:
+            for x, y in zip(self.target_x, self.target_y):
+                cv2.rectangle(rgb_image, (x-10, y-10), (x+10, y+10), tar_color, 4)
 
         convert_to_Qt_format = QImage(rgb_image.data, self.img_width, self.img_height, bytes_per_line,
                                             QImage.Format_RGB888)
@@ -89,7 +99,7 @@ class Js06MainWindow(Ui_MainWindow):
             self.begin = event.pos()
             self.end = event.pos()
             self.upper_left = (int((self.begin.x()/self.label_width)*self.img_width), int((self.begin.y()/self.label_height)*self.img_height))
-            print(self.begin)
+
             self.image_label.update()
 
         elif event.buttons() == Qt.RightButton:
@@ -107,20 +117,23 @@ class Js06MainWindow(Ui_MainWindow):
         """마우스 클릭이 떼질 때 발생하는 이벤트, QLabel method overriding"""
         self.end = event.pos()
         self.image_label.update()
-        print(self.end.x(), self.end.y())
         self.lower_right = (int((self.end.x()/self.label_width)*self.img_width), int((self.end.y()/self.label_height)*self.img_height))
+        self.a = self.upper_left
+        self.b = self.lower_right
+        print("시작점: ", self.upper_left)
+        print("끝점: ", self.lower_right)
         self.minrgb()
         self.isDrawing = False
 
     def minrgb(self):
         """드래그한 영역의 RGB 최솟값을 추출한다"""
-        left_y = min(self.upper_left[1], self.lower_right[1])
-        right_y = max(self.upper_left[1], self.lower_right[1])
+        up_y = min(self.upper_left[1], self.lower_right[1])
+        down_y = max(self.upper_left[1], self.lower_right[1])
 
         left_x = min(self.upper_left[0], self.lower_right[0])
         right_x = max(self.upper_left[0], self.lower_right[0])
 
-        test = self.cp_image[left_y:right_y, left_x:right_x, :]
+        test = self.cp_image[up_y:down_y, left_x:right_x, :]
 
         # 드래그한 영역의 RGB 값을 각각 추출한다.
         r = test[:, :, 0]
@@ -134,7 +147,11 @@ class Js06MainWindow(Ui_MainWindow):
 
         # RGB 값을 합한 뒤 가장 최솟값의 index를 추출한다.
         t_idx = np.where(sum_rgb == np.min(sum_rgb))
-        print(t_idx[0][0])
+        # print(t_idx[0][0] + left_x, t_idx[1][0] + up_y)
+        print(left_x, up_y, right_x, down_y)
+
+        self.target_y.append(t_idx[0][0] + up_y)
+        self.target_x.append(t_idx[1][0] + left_x)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
